a8c41b99aa141235178cf583b0e847997b12d8c8
suhailhusain <suhail.husain111@gmail.com>
2026-01-20 22:43:18 +0530

    Deployement commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,36 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  productionBrowserSourceMaps: true,
+  distDir: process.env.DIST_DIR || '.next',  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pixabay.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.rocket.new',
+      },
+    ],
   },
   basePath: process.env.NODE_ENV === 'production' ? '/sumshinebysums' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/sumshinebysums/' : '',
-  
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/sumshinebysums/' : '', 
   async redirects() {
     return [
       {
@@ -16,6 +40,16 @@ const nextConfig = {
         permanent: false,
       },
     ];
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.(jsx|tsx)$/,
+      exclude: [/node_modules/],
+      use: [{
+        loader: '@dhiwise/component-tagger/nextLoader',
+      }],
+    });
+    return config;
   },
 };
 
