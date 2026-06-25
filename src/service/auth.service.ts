4b6a24b0bdda89405250e35dc5c69e478196888c
suhailhusainshaan <suhail.husain111@gmail.com>
2026-06-25 14:14:09 +0530

    profile error fixed

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -9,6 +9,28 @@ export const authService = {
     const response = await api.post('/auth/google', data);
     return response.data;
   },
+  updateProfile: async (data: Record<string, unknown>) => {
+    const response = await api.put('/admin/users/me', data);
+    return response.data;
+  },
+  editProfile: async (data: FormData) => {
+    const response = await api.put('/auth/profile', data, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+
+    return response.data;
+  },
+  uploadAvatar: async (file: File) => {
+    const formData = new FormData();
+    formData.append('avatar', file);
+
+    const response = await api.post('/admin/users/me/avatar', formData, {
+      headers: { 'Content-Type': 'multipart/form-data' },
+    });
+    return response.data;
+  },
   me: async () => {
     const response = await api.get('/auth/me');
     return response.data; // { data, message, status }
